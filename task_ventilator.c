50b7545900f6513b2d5f5bee522cb343c80e2d19
Jerry Vigil <jerry.vigil@mktneutral.com>
2019-03-15 14:03:20 -0500

    Updates to timer.

--- task_ventilator.c
+++ task_ventilator.c
@@ -1,4 +1,47 @@
-//
-// Created by jerry on 3/15/19.
-//
+#define _DEFAULT_SOURCE
 
+#include <string.h>
+#include "zhelpers.h"
+
+#define NUM_TASKS 256
+#define VENTILATOR_PORT 5557
+#define SINK_PORT 5558
+
+int main(void) {
+    void *context = zmq_ctx_new();
+
+    void *ventilator = zmq_socket(context, ZMQ_PUSH);
+    char ventilator_port[16] = {'\0'};
+    sprintf(ventilator_port, "tcp://*:%d", VENTILATOR_PORT);
+
+    void *sink = zmq_socket(context, ZMQ_PUSH);
+    char sink_port[32] = {'\0'};
+    sprintf(sink_port, "tcp://localhost:%d", SINK_PORT);
+
+    zmq_bind(ventilator, ventilator_port);
+    zmq_connect(sink, sink_port);
+
+    printf("Press <Enter> when the workers are ready: ");
+    getchar();
+    printf("Routing tasks to the workers...\n");
+
+    s_send(sink, "0");
+
+    srandom((unsigned)time(NULL));
+
+    int total_msec;
+    for (int task_nbr = 0; task_nbr < NUM_TASKS; ++task_nbr) {
+        int workload = randof(100) + 1;
+        total_msec += workload;
+        char string[10];
+        sprintf(string, "%d", workload);
+        s_send(ventilator, string);
+    }
+    printf("Total expected cost: %d ms.\n", total_msec);
+
+    zmq_close(sink);
+    zmq_close(ventilator);
+    zmq_ctx_destroy(context);
+
+    return EXIT_SUCCESS;
+}
--- uv_timer.c
+++ uv_timer.c
@@ -6,7 +6,11 @@
 #include <stdio.h>
 #include <uv.h>
 
+#define TIMEOUT_MS 4000
+#define REPEAT_MS 2000
+
 static uv_loop_t *loop;
+static uv_timer_t timer_watcher;
 
 void timer_callback() {
     printf("in the timer callback....\n");
@@ -15,9 +19,8 @@ void timer_callback() {
 int main(void) {
     loop = uv_default_loop();
 
-    uv_timer_t timer_watcher;
     uv_timer_init(loop, &timer_watcher);
-    uv_timer_start(&timer_watcher, timer_callback, 4000, 2000);
+    uv_timer_start(&timer_watcher, timer_callback, TIMEOUT_MS, REPEAT_MS);
 
     return uv_run(loop, UV_RUN_DEFAULT);
 }
