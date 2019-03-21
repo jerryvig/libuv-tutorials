bc525d26829357cc9416764ec481b07d430c2f12
Jerry Vigil <jerry.vigil@mktneutral.com>
2019-03-20 19:06:37 -0500

    updates

--- curl_uv_multi_pool.c
+++ curl_uv_multi_pool.c
@@ -28,7 +28,6 @@ typedef struct {
 } curl_multi_ez_t;
 
 static uv_loop_t *loop;
-static CURLM *curl_multi_handle;
 static uv_timer_t *timeout;
 static curl_multi_ez_t curl_multi_ez;
 
@@ -76,18 +75,12 @@ static void destroy_curl_context(curl_context_t *context) {
 }
 
 static void add_download(const char *url, int num) {
-    memory_t *buffer = (memory_t*)malloc(sizeof(memory_t));
-    buffer->memory = (char*)malloc(1);
-    buffer->size = 0;
+    int ez_pool_idx = num % POOL_SIZE;
 
-    CURL *ez = curl_easy_init();
-    curl_easy_setopt(ez, CURLOPT_WRITEFUNCTION, write_callback);
-    curl_easy_setopt(ez, CURLOPT_WRITEDATA, (void*)buffer);
-    curl_easy_setopt(ez, CURLOPT_PRIVATE, buffer);
-    curl_easy_setopt(ez, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_2_0);
-    curl_easy_setopt(ez, CURLOPT_USERAGENT, "libcurl-agent/1.0");
+    CURL *ez = curl_multi_ez.ez_pool[ez_pool_idx];
     curl_easy_setopt(ez, CURLOPT_URL, url);
-    curl_multi_add_handle(curl_multi_handle, ez);
+
+    curl_multi_add_handle(curl_multi_ez.curl_multi, ez);
     fprintf(stderr, "Added download->buffer %s\n", url);
 }
 
@@ -99,7 +92,7 @@ static void check_multi_info(void) {
     CURL *ez;
     memory_t *buffer;
 
-    while ((message = curl_multi_info_read(curl_multi_handle, &pending))) {
+    while ((message = curl_multi_info_read(curl_multi_ez.curl_multi, &pending))) {
         switch (message->msg) {
             case CURLMSG_DONE:
                 ez = message->easy_handle;
@@ -107,13 +100,23 @@ static void check_multi_info(void) {
                 curl_easy_getinfo(ez, CURLINFO_PRIVATE, &buffer);
                 fprintf(stderr, "Finished fetching %s. DONE\n", done_url);
 
-                curl_multi_remove_handle(curl_multi_handle, ez);
-                curl_easy_cleanup(ez);
                 if (buffer) {
-                    printf("buffer = \"%s\"", buffer->memory);
+                    printf("buffer = %s\n", buffer->memory);
+
+                    //You would begin processing the data here, so I think this is where
+                    // you would init threads for a work queue.
                     free(buffer->memory);
-                    free(buffer);
+                    buffer->memory = (char*)malloc(1);
+                    buffer->size = 0;
                 }
+
+                curl_multi_remove_handle(curl_multi_ez.curl_multi, ez);
+                //curl_easy_cleanup(ez);
+                //if (buffer) {
+                //    printf("buffer = \"%s\"", buffer->memory);
+                //    free(buffer->memory);
+                //    free(buffer);
+                //}
                 break;
             default:
                 fprintf(stderr, "CURLMSG default\n");
@@ -139,13 +142,13 @@ void curl_perform(uv_poll_t *req, int status, int events) {
     }
 
     context = (curl_context_t*)req->data;
-    curl_multi_socket_action(curl_multi_handle, context->sockfd, flags, &running_handles);
+    curl_multi_socket_action(curl_multi_ez.curl_multi, context->sockfd, flags, &running_handles);
     check_multi_info();
 }
 
 static void on_timeout(uv_timer_t *req) {
     int running_handles;
-    curl_multi_socket_action(curl_multi_handle, CURL_SOCKET_TIMEOUT, 0, &running_handles);
+    curl_multi_socket_action(curl_multi_ez.curl_multi, CURL_SOCKET_TIMEOUT, 0, &running_handles);
     check_multi_info();
 }
 
@@ -170,8 +173,7 @@ static int handle_socket(CURL *ez, curl_socket_t curl_sock, int action, void *us
         case CURL_POLL_OUT:
         case CURL_POLL_INOUT:
             curl_context = socketp ? (curl_context_t*)socketp : create_curl_context(curl_sock);
-            curl_multi_assign(curl_multi_handle, curl_sock, (void*)curl_context);
-
+            curl_multi_assign(curl_multi_ez.curl_multi, curl_sock, (void*)curl_context);
             if (action != CURL_POLL_IN) {
                 events |= UV_WRITABLE;
             }
@@ -186,7 +188,7 @@ static int handle_socket(CURL *ez, curl_socket_t curl_sock, int action, void *us
             if (socketp) {
                 uv_poll_stop(&((curl_context_t*)socketp)->poll_handle);
                 destroy_curl_context((curl_context_t*)socketp);
-                curl_multi_assign(curl_multi_handle, curl_sock, NULL);
+                curl_multi_assign(curl_multi_ez.curl_multi, curl_sock, NULL);
             }
             break;
         default:
@@ -198,7 +200,7 @@ static int handle_socket(CURL *ez, curl_socket_t curl_sock, int action, void *us
 
 static CURLM *create_and_init_curl_multi() {
     CURLM *multi_handle = curl_multi_init();
-    curl_multi_setopt(curl_multi_handle, CURLMOPT_MAXCONNECTS, (long)POOL_SIZE);
+    curl_multi_setopt(multi_handle, CURLMOPT_MAXCONNECTS, (long)POOL_SIZE);
     curl_multi_setopt(multi_handle, CURLMOPT_PIPELINING, CURLPIPE_MULTIPLEX);
     curl_multi_setopt(multi_handle, CURLMOPT_SOCKETFUNCTION, handle_socket);
     curl_multi_setopt(multi_handle, CURLMOPT_TIMERFUNCTION, start_timeout);
@@ -236,7 +238,7 @@ static void create_and_init_multi_ez() {
     }
 }
 
-int run_loop(char* urls[], const int url_count) {
+int run_loop(const char* urls[], const int url_count) {
     if (curl_global_init(CURL_GLOBAL_ALL)) {
         fprintf(stderr, "Failed to initialize cURL.\n");
         return 1;
@@ -250,18 +252,21 @@ int run_loop(char* urls[], const int url_count) {
     uv_timer_init(loop, timeout);
 
     //initialize the curl multi handle and set the socket and timer callbacks.
+    create_and_init_multi_ez();
 
-
-    for (int i = 0; i < url_count; ++i) {
-        fprintf(stderr, "adding \"%s\" to downloads\n", urls[i]);
+    register int i;
+    for (i = 0; i < POOL_SIZE; ++i) {
+        fprintf(stderr, "adding \"%s\" to downloads...\n", urls[i]);
         add_download(urls[i], i);
     }
 
-    fprintf(stderr, "starting the event loop run....\n");
+    fprintf(stderr, "Starting the event loop run....\n");
     uv_run(loop, UV_RUN_DEFAULT);
     fprintf(stderr, "event loop run ended...shutting down...\n");
 
-    curl_multi_cleanup(curl_multi_handle);
+    //TODO: cleanup all of the ez handles and their write-buffers here.
+
+    curl_multi_cleanup(curl_multi_ez.curl_multi);
     free(timeout);
     return 0;
 }
